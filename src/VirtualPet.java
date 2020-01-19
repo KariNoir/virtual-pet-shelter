eb34f3d90312e74dc661ba133bc3882c9cc23184
KariNoir <KariNoirm@gmail.com>
2020-01-18 23:38:36 -0500

    writing test code rewrite of the virtual pet

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,9 +1,70 @@
 
 public class VirtualPet {
 
-	//public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//private String petName;
+	private int eat;
+	private int sleep;
+	private int fun;
+	private int potty;
 
+		public VirtualPet(int eat, int sleep, int fun, int potty) {
+			this.eat = eat;
+			this.sleep = sleep;
+			this.fun = fun;
+			this.potty = potty;
+		}
+
+		public void tick() {
+			eat--;
+			sleep--;
+			fun--;
+			potty--;
+		}
+
+		public int forEat() {
+			return eat;
+		}
+
+		public int forSleep() {
+			return sleep;
+		}
+
+		public int forFun() {
+			return fun;
+		}
+
+		public int forPotty() {
+			return potty;
+		}
+
+		public void fedPet() {
+			this.eat += 3;
+			this.sleep += 1;
+			this.potty -= 4;
+		}
+
+		public void goPotty() {
+			this.eat -= 2;
+			this.fun += 2;
+			this.potty += 4;
+		}
+
+
+		public void haveFun() {
+			this.sleep -= 3;
+			this.eat -= 2;
+			this.fun += 4;
+					
+		}
+		public void getSleep() {
+			this.sleep += 2;
+			this.potty -=2;
+			this.eat += 2;
+		}
+
+
+
+	
 	}
 
 
--- src/VirtualPetTest.java
+++ src/VirtualPetTest.java
@@ -1,12 +1,30 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
 class VirtualPetTest {
+	
+VirtualPet underTest = new VirtualPet(10, 10, 10, 10); // calling default constructor
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+@Test
+public void eatShouldBe10AtStart() {
+	// act
+	int result = underTest.fedPet(); // makes a method in VP class
+	// assert
+	assertEquals(0, result);
+}
+//@Test
+//public void hungerShouldDecreaseBy1AfterEat() {
+//	// act
+//	int beforeEat = underTest.getHunger();
+//	underTest.eat(); // void - just eats
+//	int result = underTest.getHunger();
+//	// assert
+//	assertEquals(beforeEat - 1, result);
+//}
+@Test
+public void thirstShouldBe0AtStart() {
+	// act
+	int result = underTest.getThirst(); // makes a method in VP2 class
+	// assert
+	assertEquals(0, result);
+}
 }
